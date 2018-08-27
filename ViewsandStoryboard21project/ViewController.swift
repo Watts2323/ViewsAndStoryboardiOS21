c54d97e0bc3a7f4cdce56ce6c25c2cceac0dc095
Xavier <xwatts2323@Xaviers-MacBook-Pro.local>
2018-08-27 10:51:17 -0600

    Added starter project

---
+++ ViewsandStoryboard21project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViewsandStoryboard21
+//
+//  Created by Xavier on 8/27/18.
+//  Copyright © 2018 Xavier ios dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
